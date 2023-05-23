5263cf480526d1ac0f0048e16c1ac751088423d2
Gerald-Simpson <gtnsimpson@gmail.com>
2023-05-23 18:19:44 +0100

    Completed translation function and tests. Highlighting function & tests still to do.

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,191 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require('./american-to-british-titles.js');
+const britishOnly = require('./british-only.js');
 
 class Translator {
+  titleConverter(text, locale) {
+    let lowerText = text.toLowerCase();
+    if (locale === 'British to American') {
+      // For each title
+      for (const [amer, brit] of Object.entries(americanToBritishTitles)) {
+        // Find the first position of a match if there is one
+        let regex = new RegExp(brit + '{1}(?![a-zA-Z])');
+        let position = lowerText.search(regex);
+        // While there are matches for the current title in lowerText
+        while (position > -1) {
+          // Replace the title in text with the American title
+          text =
+            text.slice(0, position) +
+            amer.slice(0, 1).toUpperCase() +
+            amer.slice(1) +
+            text.slice(position + brit.length);
+          // Update lowerText
+          lowerText = text.toLowerCase();
+          // Check if there are any more matches for this title in text
+          position = lowerText.indexOf(brit + ' ');
+        }
+      }
+      return text;
+    }
+    if (locale === 'American to British') {
+      // For each title
+      for (const [amer, brit] of Object.entries(americanToBritishTitles)) {
+        // Find the first position of a match if there is one
+        let regex = new RegExp(amer + '{1}(?![a-zA-Z])');
+        let position = lowerText.search(regex);
+        // While there are matches for the current title in lowerText
+        while (position > -1) {
+          // Replace the title in text with the American title
+          text =
+            text.slice(0, position) +
+            brit.slice(0, 1).toUpperCase() +
+            brit.slice(1) +
+            text.slice(position + amer.length);
+          // Update lowerText
+          lowerText = text.toLowerCase();
+          // Check if there are any more matches for this title in text
+          position = lowerText.indexOf(amer);
+        }
+      }
+    }
+    return text;
+  }
+  spellingSwap(text, locale) {
+    let lowerText = text.toLowerCase();
+    if (locale === 'American to British') {
+      //Create an array of keys in descending length order
+      let descendingKeys = Object.keys(americanToBritishSpelling).sort(
+        function (a, b) {
+          return b.length - a.length;
+        }
+      );
+      // Loop for every entry in descendingKeys length
+      for (let i = 0; i < descendingKeys.length; i++) {
+        let amer = descendingKeys[i];
+        let brit = americanToBritishSpelling[amer];
+        // Find the first position of a match if there is one
+        let regex = new RegExp(amer + '{1}(?![a-zA-Z])');
+        let position = lowerText.search(regex);
+        // While there are matches for the current title in lowerText
+        while (position > -1) {
+          // Replace the title in text with the American title
+          text =
+            text.slice(0, position) + brit + text.slice(position + amer.length);
+          // Update lowerText
+          lowerText = text.toLowerCase();
+          // Check if there are any more matches for this title in text
+          position = lowerText.indexOf(amer);
+        }
+      }
+    }
+    if (locale === 'British to American') {
+      // Create reversed object britishTo... so that we can sort array by length & reference the object using the key
+      let britishToAmericanSpelling = {};
+      for (const [amer, brit] of Object.entries(americanToBritishSpelling)) {
+        britishToAmericanSpelling[brit] = amer;
+      }
+      //Create an array of keys in descending length order
+      let descendingKeys = Object.keys(britishToAmericanSpelling).sort(
+        function (a, b) {
+          return b.length - a.length;
+        }
+      );
+      // Loop for every entry in descendingKeys length
+      for (let i = 0; i < descendingKeys.length; i++) {
+        let brit = descendingKeys[i];
+        let amer = britishToAmericanSpelling[brit];
+        // Find the first position of a match if there is one
+        let regex = new RegExp(brit + '{1}(?![a-zA-Z])');
+        let position = lowerText.search(regex);
+        if (position > -1) {
+        }
+        // While there are matches for the current title in lowerText
+        while (position > -1) {
+          // Replace the title in text with the American title
+          text =
+            text.slice(0, position) + amer + text.slice(position + brit.length);
+          // Update lowerText
+          lowerText = text.toLowerCase();
+          // Check if there are any more matches for this title in text
+          position = lowerText.indexOf(brit);
+        }
+      }
+    }
+    return text;
+  }
+  americanOnlyConvert(text, locale) {
+    if (locale === 'British to American') {
+      return text;
+    }
+    let lowerText = text.toLowerCase();
+    //Create an array of keys in descending length order
+    let descendingKeys = Object.keys(americanOnly).sort(function (a, b) {
+      return b.length - a.length;
+    });
+    // Loop for every entry in descendingKeys length
+    for (let i = 0; i < descendingKeys.length; i++) {
+      let amer = descendingKeys[i];
+      let brit = americanOnly[amer];
+      // Find the first position of a match if there is one
+      let regex = new RegExp(amer + '{1}(?![a-zA-Z])');
+      let position = lowerText.search(regex);
+      // While there are matches for the current title in lowerText
+      while (position > -1) {
+        // Replace the title in text with the American title
+        text =
+          text.slice(0, position) + brit + text.slice(position + amer.length);
+        // Update lowerText
+        lowerText = text.toLowerCase();
+        // Check if there are any more matches for this title in text
+        position = lowerText.indexOf(amer);
+      }
+    }
+    return text;
+  }
 
+  britishOnlyConvert(text, locale) {
+    if (locale === 'American to British') {
+      return text;
+    }
+    let lowerText = text.toLowerCase();
+    //Create an array of keys in descending length order
+    let descendingKeys = Object.keys(britishOnly).sort(function (a, b) {
+      return b.length - a.length;
+    });
+    // Loop for every entry in descendingKeys length
+    for (let i = 0; i < descendingKeys.length; i++) {
+      let brit = descendingKeys[i];
+      let amer = britishOnly[brit];
+      // Find the first position of a match if there is one
+      let regex = new RegExp(brit + '{1}(?![a-zA-Z])');
+      let position = lowerText.search(regex);
+      // While there are matches for the current title in lowerText
+      while (position > -1) {
+        // Replace the title in text with the American title
+        text =
+          text.slice(0, position) + amer + text.slice(position + brit.length);
+        // Update lowerText
+        lowerText = text.toLowerCase();
+        // Check if there are any more matches for this title in text
+        position = lowerText.indexOf(brit);
+      }
+    }
+    return text;
+  }
+
+  translate(text, locale) {
+    let returnText = text;
+    returnText = this.titleConverter(text, locale);
+    returnText = this.spellingSwap(returnText, locale);
+    returnText = this.americanOnlyConvert(returnText, locale);
+    returnText = this.britishOnlyConvert(returnText, locale);
+    if (returnText.slice(0, 1).match(/[a-z]/i)) {
+      return returnText.slice(0, 1).toUpperCase() + returnText.slice(1);
+    } else {
+      return returnText;
+    }
+  }
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,151 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+let translator = new Translator();
 
 suite('Unit Tests', () => {
+  test('titleConverter test', function () {
+    assert.equal(
+      translator.translate(
+        'Mangoes are my favorite fruit.',
+        'American to British'
+      ),
+      'Mangoes are my favourite fruit.'
+    );
+    assert.equal(
+      translator.translate(
+        'I ate yogurt for breakfast.',
+        'American to British'
+      ),
+      'I ate yoghurt for breakfast.'
+    );
+    assert.equal(
+      translator.translate(
+        "We had a party at my friend's condo.",
+        'American to British'
+      ),
+      "We had a party at my friend's flat."
+    );
+    assert.equal(
+      translator.translate(
+        'Can you toss this in the trashcan for me?',
+        'American to British'
+      ),
+      'Can you toss this in the bin for me?'
+    );
+    assert.equal(
+      translator.translate('The parking lot was full.', 'American to British'),
+      'The car park was full.'
+    );
+    assert.equal(
+      translator.translate(
+        'Like a high tech Rube Goldberg machine.',
+        'American to British'
+      ),
+      'Like a high tech Heath Robinson device.'
+    );
+    assert.equal(
+      translator.translate(
+        'To play hooky means to skip class or work.',
+        'American to British'
+      ),
+      'To bunk off means to skip class or work.'
+    );
+    assert.equal(
+      translator.translate(
+        'No Mr. Bond, I expect you to die.',
+        'American to British'
+      ),
+      'No Mr Bond, I expect you to die.'
+    );
+    assert.equal(
+      translator.translate(
+        'Dr. Grosh will see you now.',
+        'American to British'
+      ),
+      'Dr Grosh will see you now.'
+    );
+    assert.equal(
+      translator.translate('Lunch is at 12:15 today.', 'American to British'),
+      'Lunch is at 12:15 today.'
+    );
 
+    assert.equal(
+      translator.translate(
+        'We watched the footie match for a while.',
+        'British to American'
+      ),
+      'We watched the soccer match for a while.',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        'Paracetamol takes up to an hour to work.',
+        'British to American'
+      ),
+      'Tylenol takes up to an hour to work.',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        'First, caramelise the onions.',
+        'British to American'
+      ),
+      'First, caramelize the onions.',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        'I spent the bank holiday at the funfair.',
+        'British to American'
+      ),
+      'I spent the public holiday at the carnival.',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        'I had a bicky then went to the chippy.',
+        'British to American'
+      ),
+      'I had a cookie then went to the fish-and-chip shop.',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        "I've just got bits and bobs in my bum bag.",
+        'British to American'
+      ),
+      "I've just got odds and ends in my fanny pack.",
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        'The car boot sale at Boxted Airfield was called off.',
+        'British to American'
+      ),
+      'The swap meet at Boxted Airfield was called off.',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate('Have you met Mrs Kalyani?', 'British to American'),
+      'Have you met Mrs. Kalyani?',
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        "Prof Joyner of King's College, London.",
+        'British to American'
+      ),
+      "Prof. Joyner of King's College, London.",
+      'to American working'
+    );
+    assert.equal(
+      translator.translate(
+        'Tea time is usually around 4 or 4.30.',
+        'British to American'
+      ),
+      'Tea time is usually around 4 or 4.30.',
+      'to American working'
+    );
+  });
 });
