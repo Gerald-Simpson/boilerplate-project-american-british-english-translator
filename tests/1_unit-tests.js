3c57d4e16070500f0459b3a598c95fb5a3acdb55
Gerald-Simpson <gtnsimpson@gmail.com>
2023-05-24 16:43:07 +0100

    Finished missing unit tests. Completed Functional tests.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,7 @@ const Translator = require('../components/translator.js');
 let translator = new Translator();
 
 suite('Unit Tests', () => {
-  test('titleConverter test', function () {
+  test('Translate Mangoes are my favorite fruit. to British English', function () {
     assert.equal(
       translator.translate(
         'Mangoes are my favorite fruit.',
@@ -13,6 +13,8 @@ suite('Unit Tests', () => {
       ),
       'Mangoes are my <span class="highlight">favourite</span> fruit.'
     );
+  });
+  test('Translate I ate yogurt for breakfast. to British English', function () {
     assert.equal(
       translator.translate(
         'I ate yogurt for breakfast.',
@@ -20,6 +22,8 @@ suite('Unit Tests', () => {
       ),
       'I ate <span class="highlight">yoghurt</span> for breakfast.'
     );
+  });
+  test("Translate We had a party at my friend's condo. to British English", function () {
     assert.equal(
       translator.translate(
         "We had a party at my friend's condo.",
@@ -27,6 +31,8 @@ suite('Unit Tests', () => {
       ),
       'We had a party at my friend\'s <span class="highlight">flat</span>.'
     );
+  });
+  test('Translate Can you toss this in the trashcan for me? to British English', function () {
     assert.equal(
       translator.translate(
         'Can you toss this in the trashcan for me?',
@@ -34,10 +40,14 @@ suite('Unit Tests', () => {
       ),
       'Can you toss this in the <span class="highlight">bin</span> for me?'
     );
+  });
+  test('Translate The parking lot was full. to British English', function () {
     assert.equal(
       translator.translate('The parking lot was full.', 'american-to-british'),
       'The <span class="highlight">car park</span> was full.'
     );
+  });
+  test('Translate Like a high tech Rube Goldberg machine. to British English', function () {
     assert.equal(
       translator.translate(
         'Like a high tech Rube Goldberg machine.',
@@ -45,6 +55,8 @@ suite('Unit Tests', () => {
       ),
       'Like a high tech <span class="highlight">Heath Robinson device</span>.'
     );
+  });
+  test('Translate To play hooky means to skip class or work. to British English', function () {
     assert.equal(
       translator.translate(
         'To play hooky means to skip class or work.',
@@ -52,6 +64,8 @@ suite('Unit Tests', () => {
       ),
       'To <span class="highlight">bunk off</span> means to skip class or work.'
     );
+  });
+  test('Translate No Mr. Bond, I expect you to die. to British English', function () {
     assert.equal(
       translator.translate(
         'No Mr. Bond, I expect you to die.',
@@ -59,6 +73,8 @@ suite('Unit Tests', () => {
       ),
       'No <span class="highlight">Mr</span> Bond, I expect you to die.'
     );
+  });
+  test('Translate Dr. Grosh will see you now. to British English', function () {
     assert.equal(
       translator.translate(
         'Dr. Grosh will see you now.',
@@ -66,11 +82,14 @@ suite('Unit Tests', () => {
       ),
       '<span class="highlight">Dr</span> Grosh will see you now.'
     );
+  });
+  test('Translate Lunch is at 12:15 today. to British English', function () {
     assert.equal(
       translator.translate('Lunch is at 12:15 today.', 'american-to-british'),
       'Lunch is at <span class="highlight">12.15</span> today.'
     );
-
+  });
+  test('Translate We watched the footie match for a while. to American English', function () {
     assert.equal(
       translator.translate(
         'We watched the footie match for a while.',
@@ -79,6 +98,8 @@ suite('Unit Tests', () => {
       'We watched the <span class="highlight">soccer</span> match for a while.',
       'to American working'
     );
+  });
+  test('Translate Paracetamol takes up to an hour to work. to American English', function () {
     assert.equal(
       translator.translate(
         'Paracetamol takes up to an hour to work.',
@@ -87,6 +108,8 @@ suite('Unit Tests', () => {
       '<span class="highlight">Tylenol</span> takes up to an hour to work.',
       'to American working'
     );
+  });
+  test('Translate First, caramelise the onions. to American English', function () {
     assert.equal(
       translator.translate(
         'First, caramelise the onions.',
@@ -95,6 +118,8 @@ suite('Unit Tests', () => {
       'First, <span class="highlight">caramelize</span> the onions.',
       'to American working'
     );
+  });
+  test('Translate I spent the bank holiday at the funfair. to American English', function () {
     assert.equal(
       translator.translate(
         'I spent the bank holiday at the funfair.',
@@ -103,6 +128,8 @@ suite('Unit Tests', () => {
       'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.',
       'to American working'
     );
+  });
+  test('Translate I had a bicky then went to the chippy. to American English', function () {
     assert.equal(
       translator.translate(
         'I had a bicky then went to the chippy.',
@@ -111,6 +138,8 @@ suite('Unit Tests', () => {
       'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.',
       'to American working'
     );
+  });
+  test("Translate I've just got bits and bobs in my bum bag. to American English", function () {
     assert.equal(
       translator.translate(
         "I've just got bits and bobs in my bum bag.",
@@ -119,6 +148,8 @@ suite('Unit Tests', () => {
       'I\'ve just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.',
       'to American working'
     );
+  });
+  test('Translate The car boot sale at Boxted Airfield was called off. to American English', function () {
     assert.equal(
       translator.translate(
         'The car boot sale at Boxted Airfield was called off.',
@@ -127,11 +158,15 @@ suite('Unit Tests', () => {
       'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.',
       'to American working'
     );
+  });
+  test('Translate Have you met Mrs Kalyani? to American English', function () {
     assert.equal(
       translator.translate('Have you met Mrs Kalyani?', 'british-to-american'),
       'Have you met <span class="highlight">Mrs.</span> Kalyani?',
       'to American working'
     );
+  });
+  test("Translate Prof Joyner of King's College, London. to American English", function () {
     assert.equal(
       translator.translate(
         "Prof Joyner of King's College, London.",
@@ -140,12 +175,52 @@ suite('Unit Tests', () => {
       '<span class="highlight">Prof.</span> Joyner of King\'s College, London.',
       'to American working'
     );
+  });
+  test('Translate Tea time is usually around 4 or 4.30. to American English', function () {
     assert.equal(
       translator.translate(
         'Tea time is usually around 4 or 4.30.',
         'british-to-american'
       ),
       'Tea time is usually around 4 or <span class="highlight">4:30</span>.',
+      'to American working'
+    );
+  });
+  test('Highlight translation in Mangoes are my favorite fruit.', function () {
+    assert.equal(
+      translator.translate(
+        'Mangoes are my favorite fruit.',
+        'american-to-british'
+      ),
+      'Mangoes are my <span class="highlight">favourite</span> fruit.'
+    );
+  });
+  test('Highlight translation in I ate yogurt for breakfast.', function () {
+    assert.equal(
+      translator.translate(
+        'I ate yogurt for breakfast.',
+        'american-to-british'
+      ),
+      'I ate <span class="highlight">yoghurt</span> for breakfast.'
+    );
+  });
+  test('Highlight translation in We watched the footie match for a while.', function () {
+    assert.equal(
+      translator.translate(
+        'We watched the footie match for a while.',
+        'british-to-american'
+      ),
+      'We watched the <span class="highlight">soccer</span> match for a while.',
+      'to American working'
+    );
+  });
+  test('Highlight translation in Paracetamol takes up to an hour to work.', function () {
+    assert.equal(
+      translator.translate(
+        'Paracetamol takes up to an hour to work.',
+        'british-to-american'
+      ),
+      '<span class="highlight">Tylenol</span> takes up to an hour to work.',
       'to American working'
     );
   });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,101 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
-
+  test('Translation with text and locale fields: POST request to /api/translate', function (done) {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        text: "Prof Joyner of King's College, London.",
+        locale: 'british-to-american',
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          text: "Prof Joyner of King's College, London.",
+          translation:
+            '<span class="highlight">Prof.</span> Joyner of King\'s College, London.',
+        });
+      });
+    done();
+  });
+  test('Translation with text and invalid locale field: POST request to /api/translate', function (done) {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        text: "Prof Joyner of King's College, London.",
+        locale: 'british-to-americn',
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          error: 'Invalid value for locale field',
+        });
+      });
+    done();
+  });
+  test('Translation with missing text field: POST request to /api/translate', function (done) {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        locale: 'british-to-american',
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          error: 'No text to translate',
+        });
+      });
+    done();
+  });
+  test('Translation with missing locale field: POST request to /api/translate', function (done) {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        text: "Prof Joyner of King's College, London.",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          error: 'Required field(s) missing',
+        });
+      });
+    done();
+  });
+  test('Translation with empty text: POST request to /api/translate', function (done) {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        text: '',
+        locale: 'british-to-american',
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          error: 'No text to translate',
+        });
+      });
+    done();
+  });
+  test('Translation with text that needs no translation: POST request to /api/translate', function (done) {
+    chai
+      .request(server)
+      .post('/api/translate')
+      .send({
+        text: 'Test that needs no translation',
+        locale: 'british-to-american',
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          text: 'Test that needs no translation',
+          translation: 'Everything looks good to me!',
+        });
+      });
+    done();
+  });
 });
